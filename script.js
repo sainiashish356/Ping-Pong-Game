4cc1e401400baa26ab94a990fc132e536f43b1b0
Ashish <sainiashish356@gmail.com>
2022-06-27 03:11:06 +0530

    added reset game functionality

--- script.js
+++ script.js
@@ -14,6 +14,55 @@ let gameon = false;
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
+(function() {
+
+    rod = localStorage.getItem(storeName);
+    maxScore= localStorage.getItem(storeScore);
+
+    if(rod == "null" || maxScore == "null"){
+        alert("This is the first time you are playing...LETS STRT");
+        maxScore = 0;
+        rod = "Rod1";
+    }else{
+        alert(rod + "has max score of" + maxScore*100);
+    }
+    resetBoard(rod);
+
+})();
+
+
+function resetBoard(rodName){
+    rod1.style.left = (window.innerWidth - rod1.offsetWidth) /2 + 'px';
+    rod2.style.left = (window.innerWidth - rod2.offsetWidth) /2 + 'px';
+    ball.style.left = (window.innerWidth - ball.offsetWidth) / 2 + 'px';
+
+    //losing player gets the ball
+    if(rodname == rod2Name){
+        ball.style.top = (rod1.offsetTop + rod1.offsetHeight) + 'px';
+        ballSpeedY= 2;
+    }else if(rodname == rod1Name){
+        ball.style.top = (rod2.offsetTop + rod2.offsetHeight) + 'px';
+        ballSpeedY = -2;
+    }
+    score = 0;
+    gameon = false;
+}
+
+
+function storeWin(rod , score){
+    if(score > maxScore){
+        maxScore = score;
+        localStorage.setItem(storeName , rod);
+        localStorage.setItem(storeScore , maxScore);
+    }
+
+    clearInterval(movement);
+    resetBoard(rod);
+
+    alert(rod + "wins with the score of " + (score * 100) + "max Score is:" + (maxScore*100));
+
+}
+
 window.addEventListener('keypress' , function(event){
 
     let rodSpeed = 20;
@@ -69,7 +118,7 @@ window.addEventListener('keypress' , function(event){
 
                     //check if game ends
                     if((ballPos < rod1X) || (ballPos > (rod1X + rod1Width))){
-                        storeWin(rode2Name , score);
+                        storeWin(rod2Name , score);
                     }
                 }
                 //check for ball 2
